9b296a626f0144a580161cf5d2acb6552b636583
mauric <mauric.caceres@gmail.com>
2016-12-04 19:29:53 +0100

    adding modif for presentate in the report

--- OctaveNeurons/rs4.m
+++ OctaveNeurons/rs4.m
@@ -2,12 +2,23 @@
 %
 % Author : Mauricio Caceres
 % Version : RN_STANDART_V0
-% Comments : add mu (taux d'apprentissage) variable
+% Comments : add optimisation and temps mesure
 
 %% TODO checklist
 %=================================================
-% - add mu variable -- DONE
-% - make some graphics
+% - add temps mesure --DONE
+% - add optmissations
+% -o Shuffle the training set so that successive training examples never (rarely) belong the same class
+% o Present input examples with nearly equal frequencies per classes (ça c’est fait !)
+% o Entropie : Present input examples that produce a large error more frequently than
+%           examples that produce a small error. However, one must be careful when
+%           perturbating the normal frequencies of input samples
+% o Shuffle the training set so that successive training examples never (rarely) belong to
+%           the same class
+% o Entropie : Present input examples that produce a large error more frequently than
+%         examples that produce a small error. However, one must be careful when
+%         perturbating the normal frequencies of input samples
+
 %=================================================
 %
 % You can fork this projet in
@@ -21,12 +32,13 @@ display('//================================================================//')
 display('//                    RECHERCHE OPERATIONNELLE                    //')
 display('//================================================================//')
 display('')
-display('//============================ RS4 ===============================//')
-display('// MLP : mu variable, special Weight initialisation               //')
+display('//=========================== RS 3================================//')
+display('// MLP : mu variable, initialisation de poids selon distr. normal//')
 display('//================================================================//')
 
-
-%% Exercise 2 - S�lection des attibuts de la base de donn�s d'apprentissage
+%% --------------------
+%% TRAINING DATA BASE
+%% --------------------
 in = 1;
 attributs = zeros(4097,60); % TODO change this init set, calculate it!
 Nfft = 8912;
@@ -43,7 +55,7 @@ for numfich = 1:20
     end
 end
 %% CALCUL DES ATTRIBUTS
-%donn�es sans normalisation
+%donnees sans normalisation
  % figure(1)
  % plot(attributs);figure(gcf);
  % title('attributs non normalises')
@@ -57,6 +69,7 @@ end
  % title('attributs non normalises')
 % close all
 
+
 sonsUn = (1:3:60);
 sonsDeux = (2:3:60);
 sonsTrois = (3:3:60);
@@ -66,28 +79,54 @@ t(1:3:end) = 1+3*(randperm(20) -1);
 t(2:3:end) = 2+3*(randperm(20) -1);
 t(3:3:end) = 3+3*(randperm(20) -1);
 
+
+%% --------------------
+%% TEST DATA BASE
+%% --------------------
+
+%read data base
+in = 1;
+data_test = zeros(4097,15);
+Nfft = 8912;
+for numfich = 1:5
+    for typeson = 1:3
+        name = ['TEST_',num2str(typeson),'_',num2str(numfich),'.wav']; %'TEST_2_1.wav'
+        [dataTest,fs,Nbits] = wavread(name);%lecture de fichier wav
+        % calcul des data_test
+        L = size(dataTest,1);
+        X = fft(dataTest.*hamming(L),Nfft);
+        Sxx = 1/Nfft*abs(X).^2;
+        data_test(:,in) = Sxx(1:size(data_test,1));
+        in=in+1;
+    end
+end
+
+%normalise data
+for ligne = 1:size(data_test,1)
+    ecart = sqrt(var(data_test(ligne,:)));
+    data_test(ligne,:) = (data_test(ligne,:)-mean(data_test(ligne,:)))/ecart;
+end
+epsilon_test=zeros(1,15); %erreur pour chaque example presenté
+input_test = [ones(1,15); data_test];
+
+%% --------------------
+%% OTHERS PARAMETERS
+%% --------------------
+
 L_in = 4097+1;%on rajoute le bias
 L_cachee = 100+1;%on rajoute le bias
 L_out = 3;
-
 
 %Init parametres de distribution de proba. poids
 sigma1=1/sqrt(L_in);
 sigma2 = 1/sqrt(L_cachee);
 
-%just a test
-a=-0.5;
-b = 0.5;
-
 %creation the matrices de poids
-C = normrnd (0, sigma1, L_in, L_cachee); 
+C = normrnd (0, sigma1, L_in, L_cachee);
 W =   normrnd(0, sigma2, L_cachee, L_out);
-
-
 C_init=C;
 W_init= W;
 ym = zeros(3,1);
-
 
 %Init taux d'apprentissage
 alpha1 = .5;
@@ -96,9 +135,18 @@ muo = 0.6
 mu1 = muo/(1+alpha1*1);
 mu2 = muo/(1+alpha2*1);
 
-%% --------------------
+tt = zeros(15,1);
+
+tt(1:3:end) = 1+3*(randperm(5) -1);
+tt(2:3:end) = 2+3*(randperm(5) -1);
+tt(3:3:end) = 3+3*(randperm(5) -1);
+
+%% ------------------------------
+%
 %  ALGORITHME GENERAL
-%% --------------------
+%
+%% ------------------------------
+
 %general variables
 epsilon=zeros(1,60); %erreur en chaque iteration
 input = [ones(1,60); attributs];
@@ -108,14 +156,25 @@ boucle=1;
 i = 1;
 more off;
 
+
+
+
 tic; % init counter time
+%% --------------------
+%  ITERATIONS LOOP
+%% --------------------
 while(boucle==1)
     %reorganisation des examples
     t(1:3:end) = 1+3*(randperm(20) -1);
     t(2:3:end) = 2+3*(randperm(20) -1);
     t(3:3:end) = 3+3*(randperm(20) -1);
+    %reorganisation examples de test
+    tt(1:3:end) = 1+3*(randperm(5) -1);
+    tt(2:3:end) = 2+3*(randperm(5) -1);
+    tt(3:3:end) = 3+3*(randperm(5) -1);
 
     for i = 1:60
+
         %% calcul de sortie de couchee cachee
         vj = C'*input(:,t(i));
         r =sigmoide(vj);
@@ -136,15 +195,14 @@ while(boucle==1)
             ym = [0 1 0]';
         end
 
-        %  em = y - ym;
          em = ym - y;
         epsilon(i) = (0.5/L_out)*em'*em;
         global_error_evolution(iter) = epsilon(i);
-        % fprintf('error : %f\n', epsilon(i));
+
         % Algorihme de mise � jour des poids de sortie
         deltam_out = em.*gprime(zm);
         for neurone = 1: L_out
-            W(:,neurone) = W(:,neurone)+ ((mu1/L_out)*(deltam_out(neurone)*r));
+            W(:,neurone) = W(:,neurone) + ((mu1/L_out)*(deltam_out(neurone)*r));
         end
 
         % Algorihme de mise � jour des poids d'entr�e
@@ -159,24 +217,68 @@ while(boucle==1)
           deltaj_out(neurone) = gprime(vj(neurone))*somme_delta_w(neurone);
           C(:,neurone) = C(:,neurone) + (mu2/L_out).*input(:,t(i))*deltaj_out(neurone);
         end
-        %update step
+    end %fin boucle de presentation d'examples
+
+        %update mu
         mu1 = muo/(1+alpha1*i);
         mu2 = muo/(1+alpha2*i);
         storemu(i)=mu1; %store for graphs
         storemu2(i) = mu2;
- %pause
+        %calcule eqm pour phase de training
+        error = sum(epsilon)/60
+        eqm(iter) = error;
+
+    %% --------------------
+    %  TEST LOOP
+    %% --------------------
+    for i = 1:15
+        %% calcul de sortie de couchee cachee
+        vj_test = C'*input_test(:,tt(i));
+        r_test =sigmoide(vj_test);
+        r_test(1)=-1;%set a bias
+        %% calcul de sortie de couchee sortie
+        zm_test = W'*r_test;
+        % pause
+        y_test = sigmoide(zm_test);
+
+        %% calcul de l'erreur
+        typeson = rem(tt(i),3);
+        if typeson == 0
+            ym_test = [0 0 1]';
+        elseif typeson==1
+            ym_test = [1 0 0]';
+         else
+            ym_test = [0 1 0]';
+        end
+        em_test = ym_test - y_test;
+        epsilon_test(i) = (0.5/L_out)*em_test'*em_test;
+        global_error_evolution_test(iter) = epsilon_test(i);
+
     end
     %je calcule un "error" pour l'afficher aussi
-    error = sum(epsilon)/60
-    %fprintf('error : %f\n', error);
-    %fflush(stdout);
+    error_test = sum(epsilon_test)/15
+    eqm_test(iter) = error_test;
+    epsilon_test=zeros(1,15);
+
+    %reset epsilon
+    epsilon=zeros(1,60);
+    epsilon_test=zeros(1,60);
 
     %parametre critere pour la fin de l'ALGORITHME
-    eqm(iter) = error;
-    iter = iter +1; %counter associate to the while loop
     if(error<=e)
-        boucle = 0;
-    end
+          boucle = 0;
+      endif
+      if(error<=0.001)
+          alpha = 0.4;
+      endif
+      if(error<=0.0001)
+          alpha = 0.5;
+      endif
+      if(error<=0.00002)
+      alpha = 0.9;
+    endif
+
+    iter = iter +1; %counter associate to the while loop
 end
 elapsed_time = toc
 
@@ -189,9 +291,11 @@ xlabel('iterations','FontSize',12);
 ylabel('Error','FontSize',12);
 
 figure(4)
-plot(global_error_evolution,'LineWidth',2)
+plot(storemu1,'LineWidth',2)
 grid()
-title('Iteration error global evolution (error in each example) ','FontSize',12);
+hold (on)
+plot(storemu2,'LineWidth',2)
+title('mu','FontSize',12);
 xlabel('iterations','FontSize',12);
 ylabel('Error','FontSize',12);
 
@@ -211,22 +315,24 @@ title('Final Weights ','FontSize',12);
 xlabel('Weight id','FontSize',12);
 ylabel('Weight value','FontSize',12);
 
+%% --------------------
+%  TEST GRAPHs
+%% --------------------
 
-%GRAPH : evolution de taux d'apprentissage
-figure(7)
-plot(storemu1,'-o','LineWidth',2)
-hold (on)
-plot(storemu2,'-ob','LineWidth',2)
+
+figure(10)
+  hold on
+plot(eqm_test,'-b','LineWidth',2);
+plot(eqm,'-b','LineWidth',2);
+title('Error test ','FontSize',12);
+xlabel('iterations','FontSize',12);
+ylabel('Error','FontSize',12);
+hold off
+
+figure(12)
+plot(global_error_evolution_test,'LineWidth',2)
 grid()
-title('Taux d apprentissage ','FontSize',12);
-xlabel('Weight id','FontSize',12);
-ylabel('Weight value','FontSize',12);
-
-
-
-
-%% DOCUMENTATION
-
-
-
+title('Iteration error global evolution test (error in each example) ','FontSize',12);
+xlabel('iterations','FontSize',12);
+ylabel('Error','FontSize',12);
 %% FIN

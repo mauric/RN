0da0a8d6c25bbcf23c8d328c8ea6035b5020469b
mauric <mauric.caceres@gmail.com>
2016-12-04 19:29:27 +0100

    adapting for documentate ir

--- OctaveNeurons/rs.m
+++ OctaveNeurons/rs.m
@@ -5,18 +5,6 @@
 % Comments : fonctionnement normal du reseux, sans aucun optimisation
 
 %% TODO checklist
-%=================================================
-%  - enregistrer de nouveau les audios --DONE
-%  - calculer les attributs           --DONE
-%  - verifier: normalisation, taille de matrices etc --DONE
-% - faire une fichier fonction sigmoide       --DONE
-% - faire une fichier function gprime         --DONE
-% - bug valeur NaN in console   --DONE
-% - bug : la sortie ne change pas -- DONE
-% - bug : sortie toujour a +-0.50   --DONE
-% - refaire  initialisation de poids -- DONE
-% - verifier matrice de Hamming     -- DONE
-%=================================================
 %
 % You can fork this projet in
 %
@@ -29,8 +17,10 @@ display('//================================================================//')
 display('//                    RECHERCHE OPERATIONNELLE                    //')
 display('//================================================================//')
 
+%% --------------------
+%% TRAINING DATA BASE
+%% --------------------
 
-%% Exercise 2 - S�lection des attibuts de la base de donn�s d'apprentissage
 in = 1;
 attributs = zeros(4097,60); % TODO change this init set, calculate it!
 Nfft = 8912;
@@ -46,20 +36,40 @@ for numfich = 1:20
         in=in+1;
     end
 end
-%% CALCUL DES ATTRIBUTS
-%donn�es sans normalisation
- % figure(1)
- % plot(attributs);figure(gcf);
- % title('attributs non normalises')
-%donn�es normalise
+
 for ligne = 1:size(attributs,1)
     ecart = sqrt(var(attributs(ligne,:)));
     attributs(ligne,:) = (attributs(ligne,:)-mean(attributs(ligne,:)))/ecart;
 end
-% figure(2)
- % plot(attributs);figure(gcf);
- % title('attributs non normalises')
-% close all
+
+%% --------------------
+%% TEST DATA BASE
+%% --------------------
+
+%read data base
+in = 1;
+data_test = zeros(4097,15);
+Nfft = 8912;
+for numfich = 1:5
+    for typeson = 1:3
+        name = ['TEST_',num2str(typeson),'_',num2str(numfich),'.wav']; %'TEST_2_1.wav'
+        [dataTest,fs,Nbits] = wavread(name);%lecture de fichier wav
+        % calcul des data_test
+        L = size(dataTest,1);
+        X = fft(dataTest.*hamming(L),Nfft);
+        Sxx = 1/Nfft*abs(X).^2;
+        data_test(:,in) = Sxx(1:size(data_test,1));
+        in=in+1;
+    end
+end
+
+%normalise data
+for ligne = 1:size(data_test,1)
+    ecart = sqrt(var(data_test(ligne,:)));
+    data_test(ligne,:) = (data_test(ligne,:)-mean(data_test(ligne,:)))/ecart;
+end
+epsilon_test=zeros(1,15); %erreur pour chaque example presenté
+input_test = [ones(1,15); data_test];
 
 sonsUn = (1:3:60);
 sonsDeux = (2:3:60);
@@ -90,7 +100,14 @@ e = 1e-4;
 iter =1;
 boucle=1;
 
-%pause
+
+tt = zeros(15,1);
+
+tt(1:3:end) = 1+3*(randperm(5) -1);
+tt(2:3:end) = 2+3*(randperm(5) -1);
+tt(3:3:end) = 3+3*(randperm(5) -1);
+
+
 i = 1;
 more off;
 while(boucle==1)
@@ -145,13 +162,46 @@ while(boucle==1)
         end
  %pause
     end
-    %je calcule un "error" pour l'afficher aussi
     error = sum(epsilon)/60
-    %fprintf('error : %f\n', error);
-    %fflush(stdout);
-
-    %parametre critere pour la fin de l'ALGORITHME
     eqm(iter) = error;
+
+    %% --------------------
+    %  TEST LOOP
+    %% --------------------
+    for i = 1:15
+        %% calcul de sortie de couchee cachee
+        vj_test = C'*input_test(:,tt(i));
+        r_test =sigmoide(vj_test);
+        r_test(1)=-1;%set a bias
+        %% calcul de sortie de couchee sortie
+        zm_test = W'*r_test;
+        % pause
+        y_test = sigmoide(zm_test);
+
+        %% calcul de l'erreur
+        typeson = rem(tt(i),3);
+        if typeson == 0
+            ym_test = [0 0 1]';
+        elseif typeson==1
+            ym_test = [1 0 0]';
+         else
+            ym_test = [0 1 0]';
+        end
+        em_test = ym_test - y_test;
+        epsilon_test(i) = (0.5/L_out)*em_test'*em_test;
+        global_error_evolution_test(iter) = epsilon_test(i);
+
+    end
+    %je calcule un "error" pour l'afficher aussi
+    error_test = sum(epsilon_test)/15
+    eqm_test(iter) = error_test;
+    epsilon_test=zeros(1,15);
+
+    %reset epsilon
+    epsilon=zeros(1,60);
+    epsilon_test=zeros(1,60);
+
+
     iter = iter +1;
     if(error<=e)
         boucle = 0;
@@ -159,15 +209,51 @@ while(boucle==1)
 end
 
 
-%% calcul de Erreur quadratique moyenne
+
+
+%% --------------------
+%  TRAIN GRAPHS
+%% --------------------
+figure(1)
 plot(eqm,'-b','LineWidth',2);
-title('Error ','FontSize',12);
+hold on;
+plot(eqm_test,'-r','LineWidth',2);
+grid ();
+title('Error EQM in training and test phase','FontSize',12,'FontName','5');
+xlabel('iterations','FontSize',12);
+ylabel('Error value','FontSize',12);
+legend('EQM train','EQM test');
+
+
+figure(5)
+subplot (2, 1, 1)
+plot(W_init,'-o','LineWidth',2)
+axis ([0 120 -0.8 0.8])
+grid()
+title('Initial Weights (Normal distribution initialisation) ','FontSize',12);
+xlabel('Weight id','FontSize',12);
+ylabel('Weight value','FontSize',12);
+subplot (2, 1, 2)
+plot(W,'-o','LineWidth',2)
+axis ([0 120 -0.8 0.8])
+grid()
+title('Final Weights ','FontSize',12);
+xlabel('Weight id','FontSize',12);
+ylabel('Weight value','FontSize',12);
+
+%% --------------------
+%  TEST GRAPHs
+%% --------------------
+
+
+figure(12)
+plot(global_error_evolution_test,'LineWidth',2)
+grid()
+title('Iteration error global evolution test (error in each example) ','FontSize',12);
 xlabel('iterations','FontSize',12);
 ylabel('Error','FontSize',12);
-
-
-%% DOCUMENTATION
-
-
-
 %% FIN
+
+%% --------------------
+%  DATA BASE GRAPH
+%% --------------------
